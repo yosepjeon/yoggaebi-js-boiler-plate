e743980ff490f9a17e0bf1569e62f5c693f45aa2
요깨비 <yoggaebi@yokkaebiui-MacBookPro.local>
2020-11-05 01:06:52 +0900

    로그인, token 생성

--- index.js
+++ index.js
@@ -5,12 +5,14 @@ const mongoose = require('mongoose');
 const {User} = require('./models/User');
 const bodyParser = require('body-parser');
 const config = require('./config/key');
+const cookieParser = require('cookie-parser')
 
 // application/x-www-form-urlencoded <- 이렇게 된 데이터들을 분석해서 가져올 수 있게 해줌
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json 데이터를 가져올 수 있게 해줌
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 mongoose.connect(config.mongoURI,{
     useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
@@ -33,5 +35,42 @@ app.post('/register',(req,res) => {
         }
     });
 });
+
+app.post('/api/users/login', (req, res) => {
+
+    // console.log('ping')
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+  
+      // console.log('user', user)
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "제공된 이메일에 해당하는 유저가 없습니다."
+        })
+      }
+  
+      //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+      console.log(user.password);
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        // console.log('err',err)
+  
+        // console.log('isMatch',isMatch)
+  
+        if (!isMatch)
+          return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+  
+        //비밀번호 까지 맞다면 토큰을 생성하기.
+        user.generateToken((err, user) => {
+          if (err) return res.status(400).send(err);
+  
+          // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+          res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+        })
+      })
+    })
+  })
 
 app.listen(port,() => console.log(`Example app listening on port ${port}`));
